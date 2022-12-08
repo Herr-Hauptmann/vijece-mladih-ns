9b0de6b945188cd75f91e514340142dff6cf7497
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-12-09 00:23:19 +0100

    create login with jwt

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const { check, validationResult } = require("express-validator");
-const {sequelize, User} = require('../models');
+const { sequelize, User } = require("../models");
 const bcrypt = require("bcrypt");
 const JWT = require("jsonwebtoken");
-const env = require(__dirname + '/../env.json');
+const env = require(__dirname + "/../env.json");
 
 router.post(
     "/signup",
@@ -12,10 +12,9 @@ router.post(
         check("password", "Unesena lozinka nije duža od 5 karaktera").isLength({
             min: 6,
         }),
-        check("name", "Niste unijeli ime").isLength({min:1})
+        check("name", "Niste unijeli ime").isLength({ min: 1 }),
     ],
-    async (req, res) =>{
-
+    async (req, res) => {
         //Validate the input
         let { password, email, name } = req.body;
         const errors = validationResult(req);
@@ -26,45 +25,86 @@ router.post(
         //Check if user already exists
         const exists = await User.count({
             where: {
-                email: email
-            }
+                email: email,
+            },
         });
 
         if (exists)
-            return res.status(400).json({ errors: [
+            return res.status(400).json({
+                errors: [
                     {
-                        "value" : email,
-                        "msg" : "Korisnik s ovim emailom već postoji",
-                        "param" : "email",
-                        "location" : "body"
-                    }
-                ] 
+                        value: email,
+                        msg: "Korisnik s ovim emailom već postoji",
+                        param: "email",
+                        location: "body",
+                    },
+                ],
             });
 
         //Hash password
         password = await bcrypt.hash(password, 10);
-        
+
         //Create user
-        try{
+        try {
             const user = User.create({
-                email:email,
-                password:password,
-                name: name
+                email: email,
+                password: password,
+                name: name,
             });
 
-            const token = await JWT.sign({
-                name
-            },env.secret, {
-                expiresIn:9000
-            })
+            const token = JWT.sign(
+                {
+                    email,
+                },
+                env.secret,
+                {
+                    expiresIn: 9000,
+                }
+            );
             //NOTE: Malo promisliti trajanje i refreshovanje sesije
             res.json({
-                token
+                token,
             });
-        }catch(err){
-            return res.status(500).json({err});
-        } 
+        } catch (err) {
+            return res.status(500).json({ err });
+        }
     }
 );
+
+router.post("/login", async (req, res) => {
+    let { password, email } = req.body;
+    try {
+        let user = await User.findOne({
+            where: {
+                email: email,
+            },
+        });
+        if (!user) throw err;
+
+        let isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) throw err;
+        const token = JWT.sign(
+            {
+                email
+            },
+            env.secret,
+            {
+                expiresIn: 9000,
+            }
+        );
+        //NOTE: Malo promisliti trajanje i refreshovanje sesije
+        res.json({
+            token,
+        });
+    } catch (err) {
+        return res.status(404).json({
+            errors: [
+                {
+                    msg: "Uneseni email ili lozinka nisu tačni",
+                },
+            ],
+        });
+    }
+});
 
 module.exports = router;
--- server/routes/users.js
+++ server/routes/users.js
@@ -12,38 +12,4 @@ router.get('/',async (req, res)=>{
     }
 });
 
-//CREATE
-router.post('/', async(req, res)=>{
-    const {
-        name,
-        email,
-        password,
-    } = req.body;
-    try{
-        const user = await User.create({
-            name,
-            email,
-            password,
-        });
-        return res.json(user);
-    }
-    catch(err){
-        console.log(err);
-    }
-})
-
-//FIND
-router.get('/:uuid',async (req, res)=>{
-    const uuid = req.params.uuid;
-    try{
-        const user = await User.findOne({
-            where: {uuid : uuid},
-        });
-        return res.json(user);
-    } catch(err){
-        console.log(err);
-        return res.status(500).json({error:"Greška pri dobavljanju korisnika."})
-    }
-});
-
 module.exports = router;
