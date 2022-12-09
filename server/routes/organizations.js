b8f9255bb5da6ca2e09063f7a8ae196a9d55fedd
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-12-10 00:29:37 +0100

    add crud methods to organizations controller

--- server/routes/organizations.js
+++ server/routes/organizations.js
@@ -1,11 +1,17 @@
 const router = require("express").Router();
 const {sequelize, Organization} = require('../models');
 
-
-router.get('/', (req, res)=>{
-    //Vrati sve organizacije
+//Index
+router.get('/', async (req, res)=>{
+    try{
+        const organizations = await Organization.findAll();
+        return res.json(organizations);
+    } catch(err){
+        return res.status(500).json({error:"Greška pri dobavljanju organizacija."})
+    }
 });
 
+//Create
 router.post('/', async(req, res)=>{
     const {
         name,
@@ -29,7 +35,53 @@ router.post('/', async(req, res)=>{
     catch(err){
         console.log(err);
     }
-})
+});
 
+//READ
+router.get('/:id', async (req, res)=>{
+    const id = req.params.id;
+    try{
+        const organization = await Organization.findOne({
+            where: {id}
+        });
+        return res.json(organization);
+    } catch(err){
+        return res.status(500).json({error:"Greška pri dobavljanju organizacija."})
+    }
+});
+
+
+//UPDATE
+router.put('/:id', async(req, res)=>{
+    const id = req.params.id;
+    try{
+        const organization = await Organization.update(req.body, {
+            where: {
+              id
+            }
+        });
+        return res.json(organization);
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({error:"Greška pri izmjeni podataka o organizaciji."})
+    }
+});
+
+
+//DELETE
+router.delete('/:id', async(req, res)=>{
+    const id = req.params.id;
+    try{
+        await Organization.destroy({
+            where: {
+                id
+            }
+          });
+        return res.json({msg: "Organizacija uspjesno izbrisana!"});
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({error:"Greška pri brisanju organizacije."})
+    }
+});
 
 module.exports = router;
