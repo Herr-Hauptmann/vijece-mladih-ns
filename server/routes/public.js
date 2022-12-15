63f3c11671f03d67411111e8c16c173f2cb6117b
Herr-Hauptmann <tarik.horoz@gmail.com>
2022-12-15 15:17:38 +0100

    separate client code

--- server/routes/public.js
+++
@@ -1,9 +0,0 @@
-const express= require('express');
-const path = require('../app.js');
-const router = express.Router();
-
-router.get('/', (req, res, next) =>{
-    console.log(path.dirname);
-});
-
-module.exports = router;
