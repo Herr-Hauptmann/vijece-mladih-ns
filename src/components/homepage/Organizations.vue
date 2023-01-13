9c806e86f9b5212e96311c1044ad4a7fe3e7e1b7
Herr-Hauptmann <tarik.horoz@gmail.com>
2023-01-13 01:20:38 +0100

    add links to organziations

--- src/components/homepage/Organizations.vue
+++ src/components/homepage/Organizations.vue
@@ -4,16 +4,16 @@
         <h2 class="text-center title">Organizacije članice Vijeća mladih Općine Novo Sarajevo</h2>
     </div>
     <div class="row justify-content-center">
-      <a v-for="organizacija in organizacije" class="col-6 col-md-3 d-flex align-items-stretch px-4">
-          <div class="mb-3 pt-4 d-flex flex-column justify-content-around">
+      <div v-for="organizacija in organizacije" class="col-6 col-md-3 d-flex align-items-stretch px-4" >
+          <a class="mb-3 pt-4 d-flex flex-column justify-content-around" target="_blank" :href="organizacija.link">
             <div class="row justify-content-center">
                 <img class="img-fluid w-75" :src="require('../../assets/img/logotipovi/'+organizacija.logo)" :alt="organizacija.title">
             </div>  
               <div class="row justify-content-center pt-2">
                 <h5 class="org text-center name">{{organizacija.title}}</h5>
             </div>
-          </div>
         </a>
+        </div>
     </div>
    </div>
 </template>
@@ -26,32 +26,38 @@ export default {
                 {
                     title: "Udruženje za razvoj i podršku mladih Plus Ultra",
                     logo: "plusultra.png",
+                    link: "https://plusultra.ba"
                 },
                 {
                     title: "EESTEC LC Sarajevo",
                     subtitle: "EESTEC LC Sarajevo",
                     logo: "eesteclcsarajevo.png",
+                    link: "https://eestec-sa.ba"
                 },
                 {
                     title: "Youth for peace",
-                
-                    logo: "yfp.png"
+                    logo: "yfp.png",
+                    link: "https://www.youth-for-peace.ba/en/",
                 },
                 {
                     title: "Hrvački klub “Željezničar”",
                     logo: "hkzeljo.jpg",
+                    link: "https://hrvanje.ba"
                 },
                 {
                     title:"Udruženje Judo klub \"OLIMP\"",
                     logo: "jkolimp.jpg",
+                    link: "http://www.judoclubolimp.com"
                 },
                 {
                     title: "Karate klub “RESPECT”",
                     logo: "kkrespect.jpg",
+                    link: "https://karateklubrespect.ba/index"
                 },
                 {
                     title: "Omladinska razvojna asocijacija",
                     logo: "ora.jpg",
+                    link: "https://www.facebook.com/udruzenjeora"
                 },
             ]
         }
